2fc9c00639972297271af4bbe1e4deb2e94cf223
LaraGastaldi <lara.f.gastaldi@gmail.com>
2024-11-28 19:50:18 -0300

    showing debt already validated if person re-enter link

--- backend/resources/views/pages/verify-debt.blade.php
+++ backend/resources/views/pages/verify-debt.blade.php
@@ -2,6 +2,11 @@
 
 <div class="container-fluid d-flex justify-content-center align-items-center" style="background-color: #80c783; height: 100vh;">
     <div class="container bg-white border p-3 card">
+        @if($user->verified_at != null)
+            <div class="text-success my-4">
+                Olá {{ $user->name }}, sua dívida já foi validada!
+            </div>
+        @else
         <h2>Olá {{ $user->name }}, você tem uma dívida</h2>
         <p>{{ $debt->user->first_name }} registrou uma dívida em seu e-mail</p>
         <p>Valor: R$ {{ number_format($user->value ?? $debt->total_value, 2) }}</p>
@@ -17,6 +22,8 @@
             </div>
         </form>
         <p class="small text-secondary">É importante que a dívida seja validada para que você receba um lembrete, se tiver data máxima para pagar</p>
+        @endif
 
+        <p class="small text-secondary text-center">Racha Contas</p>
     </div>
 </div>
